bf579c754bedec5fa8a74c19f62870c453012fb0
Abir <abirmahmud221@gmail.com>
2023-12-30 12:04:26 +0600

    navbar responsiveness added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,11 @@ const Navbar = () => {
     return (
         <div className="border-b pb-2 md:sticky md:top-0 z-10 bg-white">
             <nav className="py-3">
-            <div className="flex justify-between items-center">
+            <div className="flex flex-col md:flex-row justify-between items-center">
                 <div className="">
                     <div className="w-40 ml-10"><img src={logo} alt="" /></div>
                 </div>
-                <ul className="flex flex-col md:flex-row lg:flex-row justify-between gap-5 text-sm">
+                <ul className="flex flex-wrap md:flex-row lg:flex-row justify-between gap-5 text-sm">
                     <li><button
                             className="px-2 py-2 md:px-2 md:py-1 lg:px-5 lg:py-2 mt-2 bg-blue-400 text-white rounded-md hover:bg-gray-100 hover:text-blue-400"><a
                                 href="#home">HOME</a></button></li>
