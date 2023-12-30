2e8768604af2a238eba05d350cf427ea6e1d5204
Abir <abirmahmud221@gmail.com>
2023-12-30 11:59:06 +0600

    distributor is added to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,22 +10,25 @@ const Navbar = () => {
                 </div>
                 <ul className="flex flex-col md:flex-row lg:flex-row justify-between gap-5 text-sm">
                     <li><button
-                            className="px-3 py-2 md:px-2 md:py-1 lg:px-9 lg:py-2 mt-2 bg-blue-400 text-white rounded-md hover:bg-gray-100 hover:text-blue-400"><a
+                            className="px-2 py-2 md:px-2 md:py-1 lg:px-5 lg:py-2 mt-2 bg-blue-400 text-white rounded-md hover:bg-gray-100 hover:text-blue-400"><a
                                 href="#home">HOME</a></button></li>
                     <li><button
-                            className="px-3 py-2 md:px-2 md:py-1 lg:px-9 lg:py-2 mt-2 bg-blue-400 text-white rounded-md hover:bg-gray-100 hover:text-blue-400"><a
+                            className="px-2 py-2 md:px-2 md:py-1 lg:px-5 lg:py-2 mt-2 bg-blue-400 text-white rounded-md hover:bg-gray-100 hover:text-blue-400"><a
                                 href="#wwd">WHAT WE DO</a></button></li>
                     <li><button
-                            className="px-3 py-2 md:px-2 md:py-1 lg:px-9 lg:py-2 mt-2 bg-blue-400 text-white rounded-md hover:bg-gray-100 hover:text-blue-400"><a
+                            className="px-2 py-2 md:px-2 md:py-1 lg:px-5 lg:py-2 mt-2 bg-blue-400 text-white rounded-md hover:bg-gray-100 hover:text-blue-400"><a
                                 href="#wwo">WHAT WE OFFER</a></button></li>
                     <li><button
-                            className="px-3 py-2 md:px-2 md:py-1 lg:px-9 lg:py-2 mt-2 bg-blue-400 text-white rounded-md hover:bg-gray-100 hover:text-blue-400"><a
+                            className="px-2 py-2 md:px-2 md:py-1 lg:px-5 lg:py-2 mt-2 bg-blue-400 text-white rounded-md hover:bg-gray-100 hover:text-blue-400"><a
                                 href="#successful">SUCCESS STORY</a></button></li>
                     <li><button
-                            className="px-3 py-2 md:px-2 md:py-1 lg:px-9 lg:py-2 mt-2 bg-blue-400 text-white rounded-md hover:bg-gray-100 hover:text-blue-400"><a
+                            className="px-2 py-2 md:px-2 md:py-1 lg:px-5 lg:py-2 mt-2 bg-blue-400 text-white rounded-md hover:bg-gray-100 hover:text-blue-400"><a
                                 href="#wwa">WHO WE ARE</a></button></li>
                     <li><button
-                            className="px-3 py-2 md:px-2 md:py-1 lg:px-9 lg:py-2 mt-2 bg-blue-400 text-white rounded-md hover:bg-gray-100 hover:text-blue-400"><a
+                            className="px-2 py-2 md:px-2 md:py-1 lg:px-5 lg:py-2 mt-2 bg-blue-400 text-white rounded-md hover:bg-gray-100 hover:text-blue-400"><a
+                                href="">DISTRIBUTORS</a></button></li>
+                    <li><button
+                            className="px-2 py-2 md:px-2 md:py-1 lg:px-5 lg:py-2 mt-2 bg-blue-400 text-white rounded-md hover:bg-gray-100 hover:text-blue-400"><a
                                 href="#cus">CONTACT US</a></button></li>
                 </ul>
             </div>
