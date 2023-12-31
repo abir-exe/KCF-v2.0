6b8692e7b1f4ab21f3c408d0beee0d44444b87e6
Abir <abirmahmud221@gmail.com>
2023-12-31 23:08:18 +0600

    distributor element added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Home/Home";
+import Distributors from "../Pages/Distributors/Distributors";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Home></Home>,
     },
+    {
+        path: "/distributors",
+        element: <Distributors></Distributors>
+    }
   ]);
 
   export default router
