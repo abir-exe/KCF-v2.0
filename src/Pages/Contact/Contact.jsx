2978c19ee2e5cebcf66bd622f1f8917c99ef60c4
Abir <abirmahmud221@gmail.com>
2024-01-01 12:44:46 +0600

    contact

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,9 +1,104 @@
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { FaLocationDot } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
 
 
 const Contact = () => {
     return (
         <div>
-            This is contact
+           <div className="text-4xl text-center bg-blue-50 w-1/3 mx-auto rounded-full mb-12 my-10 py-5 font-bold">
+          Our Locations
+        </div>
+            <nav>
+
+          <header className="footer-title mb-4 flex items-center"> Address</header>
+          <a className="link link-hover border-b"><span className="font-bold">Corporate Office:</span> <br />
+            <span className="font-semibold">KCF Co. Ltd. BD:</span> <br />  <FaLocationDot /> House 71, Road-6, West Kawlar Khilkhet, Dhaka-1229, Bangladesh</a>
+
+          <a className="link link-hover mt-3 border-b"><span className="font-semibold">Japan Office:</span>
+            <br />
+            <FaLocationDot />
+            KCF Co. Ltd. Japan <br />
+            (Seiken Medical Industry Co. Ltd.) <br />
+            1-5-15 Nishitenma, Kita-ku, Osaka-shi, Osaka, Japan, 530-0047 <br />
+            <BsFillTelephoneFill></BsFillTelephoneFill>
+            Tel: +81-6-7504-4721(land), <br />
+            Mobile : +81-90-3975-3625, +81-80-4394-3943, +81-80-1890-4924  <br />
+            Fax: 06-7504-4721
+            <br />
+            <MdEmail />
+            E-mail: h-siddiki@kcfjp.com, m-nabi@kcfjp.com
+          </a>
+          <br />
+
+          <a className="link link-hover border-b"><span className="font-bold">Partner Company:</span> <br />
+            <span className="font-semibold">PROSERVE K.K.:</span> <br />  <FaLocationDot /> Kyoto-shi, Sakyo-ku, Takano, Higashihirakicho 1-7-6-301,Japan, 606-8107 <br />
+            <span className="font-semibold">NEXTGEN SOLUTION LTD.:</span> <br />  <FaLocationDot /> 41a Maryland Road Maryland Road, London, England, E15 1JJ</a>
+
+        </nav>
+        {/* timeline  */}
+        <div>
+        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+  <li>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <div className="timeline-start md:text-end mb-10">
+      <time className="font-mono italic">1984</time>
+      <div className="text-lg font-black">First Macintosh computer</div>
+      The Apple Macintosh—later rebranded as the Macintosh 128K—is the original Apple Macintosh personal computer. It played a pivotal role in establishing desktop publishing as a general office function. The motherboard, a 9 in (23 cm) CRT monitor, and a floppy drive were housed in a beige case with integrated carrying handle; it came with a keyboard and single-button mouse.
+    </div>
+    <hr/>
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <div className="timeline-end mb-10">
+      <time className="font-mono italic">1998</time>
+      <div className="text-lg font-black">iMac</div>
+      iMac is a family of all-in-one Mac desktop computers designed and built by Apple Inc. It has been the primary part of Apple's consumer desktop offerings since its debut in August 1998, and has evolved through seven distinct forms
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <div className="timeline-start md:text-end mb-10">
+      <time className="font-mono italic">2001</time>
+      <div className="text-lg font-black">iPod</div>
+      The iPod is a discontinued series of portable media players and multi-purpose mobile devices designed and marketed by Apple Inc. The first version was released on October 23, 2001, about 8+1⁄2 months after the Macintosh version of iTunes was released. Apple sold an estimated 450 million iPod products as of 2022. Apple discontinued the iPod product line on May 10, 2022. At over 20 years, the iPod brand is the oldest to be discontinued by Apple
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <div className="timeline-end mb-10">
+      <time className="font-mono italic">2007</time>
+      <div className="text-lg font-black">iPhone</div>
+      iPhone is a line of smartphones produced by Apple Inc. that use Apple's own iOS mobile operating system. The first-generation iPhone was announced by then-Apple CEO Steve Jobs on January 9, 2007. Since then, Apple has annually released new iPhone models and iOS updates. As of November 1, 2018, more than 2.2 billion iPhones had been sold. As of 2022, the iPhone accounts for 15.6% of global smartphone market share
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <div className="timeline-start md:text-end mb-10">
+      <time className="font-mono italic">2015</time>
+      <div className="text-lg font-black">Apple Watch</div>
+      The Apple Watch is a line of smartwatches produced by Apple Inc. It incorporates fitness tracking, health-oriented capabilities, and wireless telecommunication, and integrates with iOS and other Apple products and services
+    </div>
+  </li>
+</ul>
+        </div>
         </div>
     );
 };
