cdca9491a4c65dba1496148497489076e3ca0ecd
Abir <abirmahmud221@gmail.com>
2024-01-01 12:18:25 +0600

    .

--- src/Pages/Distributors/Distributors.jsx
+++ src/Pages/Distributors/Distributors.jsx
@@ -10,36 +10,36 @@ const Distributors = () => {
         {/* timeline  */}
        <div className="grid grid-cols-3 gap-2 m-2 justify-center items-center">
         {/* cards */}
-        <div className="card  bg-base-100 shadow-xl">
-  <figure><img src="https://jsr.co.th/wp-content/uploads/2018/01/YAMAWA-logo.png" alt="Shoes" /></figure>
+        <div className="card h-96 bg-base-100 shadow-xl">
+  <figure><img className="h-40"  src="https://jsr.co.th/wp-content/uploads/2018/01/YAMAWA-logo.png" alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title">YAMAWA MFG. Co. Ltd.</h2>
-    <p>Since the foundation of 1923, YAMAWA group has been studying and developing highest quality cutting tools (Taps, Dies, Center Drills and so on) to meet with daily progressing technical needs. With great vision and spirit that we are dedicated to the development of metal working society, we are making daily efforts as a cutting tool specialist.</p>
+    <p>Since the foundation of 1923, YAMAWA group has been studying and developing highest quality cutting tools (Taps, Dies, Center Drills and so on) to meet with daily progressing technical needs.</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-outline btn-info ">Visit Website</button>
+      <button className="btn btn-outline  ">Visit Website</button>
     </div>
   </div>
 </div>
         {/* cards */}
-        <div className="card  bg-base-100 shadow-xl">
-  <figure><img src="https://www.felisattiutensili.com/wp-content/uploads/2019/04/utensili-nachi-udine-pordenone.jpg" alt="Shoes" /></figure>
+        <div className="card  h-96  bg-base-100 shadow-xl">
+  <figure><img className="h-40" src="https://www.felisattiutensili.com/wp-content/uploads/2019/04/utensili-nachi-udine-pordenone.jpg" alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title">NACHI-FUJIKOSHI CORP</h2>
     <p>Nachi-Fujikoshi Corp. is a Japanese corporation known for its industrial robots, machining tools and systems and machine components. Nachi-Fujikoshi is listed on the Tokyo Stock Exchange and as of January 2014, comprises 50 companies.</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+    <button className="btn btn-outline  ">Visit Website</button>
     </div>
   </div>
 </div>
         {/* cards */}
-        <div className="card  bg-base-100 shadow-xl">
-  <figure><img src="https://bslbatt.co.za/wp-content/uploads/2022/08/BSL-Icon.png" alt="Shoes" /></figure>
+        <div className="card  h-96  bg-base-100 shadow-xl">
+  <figure><img className="h-40"  src="https://bslbatt.co.za/wp-content/uploads/2022/08/BSL-Icon.png" alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title">BSLBATT</h2>
     <p>BSLBATT is a professional lithium-ion battery manufacturer, including R&D and OEM services for more than 20 years. The company takes the development and production of the advanced series "BSLBATT" (best solution lithium battery) as its mission.
 </p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+    <button className="btn btn-outline  ">Visit Website</button>
     </div>
   </div>
 </div>
